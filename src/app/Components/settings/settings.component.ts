218b166d53810c6bac93c65674827688ec6df330
Mariam Khreis <mariam-khreis@hotmail.com>
2024-02-19 17:56:29 +0100

    Almost full UI (cat namimg screen still missing)

--- src/app/Components/settings/settings.component.ts
+++ src/app/Components/settings/settings.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+
+interface Theme {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
-
+  themes: Theme[] = [
+    {value: 'pink-0', viewValue: 'Pink (default)'},
+    {value: 'blue-1', viewValue: 'Blue'}
+  ]
 }
